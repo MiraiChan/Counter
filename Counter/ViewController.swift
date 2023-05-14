3b0543ee04cddb237f07755455393ae97d214c12
MiraiChan <almira-garipova@mail.ru>
2023-05-14 17:56:17 +0200

    Добавила лейбл (UILabel) и кнопку (UIButton). Задала действие: по нажатию на кнопку значение счётчика меняется — и текст в лейбле тоже должен поменяться. При запуске приложения у лейбла текст должен быть 0; при каждом нажатии на кнопку это число должно увеличиваться на 1.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var countTrack: Int = 0
+    
+    @IBOutlet weak var counterValueLabel: UILabel!
+    @IBOutlet weak var startCountingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterValueLabel.text = "Значение счетчика: \(countTrack)"
+        startCountingButton.setTitle("+", for: .normal)
+        
     }
 
-
+    @IBAction func startCountingButton(_ sender: Any) {
+            self.countTrack += 1
+        counterValueLabel.text = "Значение счетчика: \(countTrack)"
+        print(self.countTrack)
+    }
+    
+  
 }
 
