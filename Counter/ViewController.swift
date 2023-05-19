3871119654968a199059272f35fc1701dd29959a
MiraiChan <almira-garipova@mail.ru>
2023-05-19 16:40:07 +0200

    Добавила UITextView, DateFormatter и активировала History  в кнопке  Reset.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -14,19 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak private var counterValueLabel: UILabel!
     @IBOutlet weak private var startCountingButton: UIButton!
     
+    @IBOutlet weak var historyTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.updateCounterLabel()
-        startCountingButton.setTitle("+", for: .normal)
+        historyTextView.text = "История изменений:\n"
+        view.addSubview(historyTextView)
+        
     }
+    
 
     @IBAction private func startCountingButton(_ sender: Any) {
         self.countTrack += 1
         self.updateCounterLabel()
     }
     
+    @IBAction func decrementCountingButton(_ sender: Any) {
+        self.countTrack -= 1
+        if countTrack < 0 {
+            countTrack = 0
+        }
+        self.updateCounterLabel()
+    }
+    @IBAction func resetButton(_ sender: Any) {
+        countTrack = 0
+        self.updateCounterLabel()
+let currentDate = getCurrentDate()
+        historyTextView.text += "\(currentDate): значение сброшено"
+        print(currentDate)
+    }
     private func updateCounterLabel() {
         counterValueLabel.text = "Значение счетчика: \(countTrack)"
+    }
+    private func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yy, HH:mm"
+        return dateFormatter.string(from: Date())
     }
 }
 
